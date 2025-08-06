c44248552f3ce17068d57054ea4b619b9327d879
root <root@bastion.jferre.local>
2025-08-06 10:37:23 -0300

    Update aide_monitoring.sh

--- aide_monitoring.sh
+++ aide_monitoring.sh
@@ -8,9 +8,6 @@ then
    SERVERNAME=`hostname`
    PATHS=`grep '^f++' /var/log/aide/aide.log | awk '{print $2}'| jq -R | jq -s`
 
-   echo $PATHS
-   
-   #curl --header "Content-Type: application/json" --request POST --data "{\"server\": \"${SERVERNAME}\", \"path_files\": $PATHS }" bastion.jferre.local:5001
    curl --header "Content-Type: application/json" --request POST --data "{\"server\": \"${SERVERNAME}\", \"path_files\": $PATHS }" eda.jferre.local:5001
 
 fi
