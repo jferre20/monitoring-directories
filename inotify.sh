d37f13ce4ee81a996b41fa5e51f8c9669915484b
root <root@bastion.jferre.local>
2025-08-11 18:46:24 -0300

    Remove token

--- inotify.sh
+++ inotify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 /usr/bin/inotifywait -e create,move -mrq /tmp/new-files | while read line; do
-DIRECTORY=`echo $line | /usr/bin/awk '{print $1}'`
-FILENAME=`echo $line | /usr/bin/awk '{print $3}'`
+DIRECTORY=`echo $line | awk '{print $1}'`
+FILE=`echo $line | awk '{print $3}'`
 
-PATH_FILE="${DIRECTORY}${FILENAME}"
+PATHFILE=${DIRECTORY}${FILE}
 
-curl -X POST https://controller.jferre.local/api/v2/job_templates/9/launch/ -H "Authorization: Bearer YOUR_TOKEN_HERE" --data "{\"extra_vars\": {\"path_file\": $PATH_FILE } }"
+curl -X POST https://controller.jferre.local/api/v2/job_templates/9/launch/ -H "Content-Type: application/json" -H "Authorization: Bearer YOUR_TOKEN_CONTROLLER_HERE" --data "{\"extra_vars\": {\"path_file\": \"$PATHFILE\" } }"
 
 done
