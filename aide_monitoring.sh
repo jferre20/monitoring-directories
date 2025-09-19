b6b363174132989100065234d5c1f825a973f332
Joao Ferreira <joaferre@redhat.com>
2025-09-18 22:16:34 -0300

    monitoring

--- aide_monitoring.sh
+++ aide_monitoring.sh
@@ -9,6 +9,6 @@ then
    PATHS=`grep '^f++' /var/log/aide/aide.log | awk '{print $2}'| jq -R | jq -s`
 
    #curl --header "Content-Type: application/json" --request POST --data "{\"server\": \"${SERVERNAME}\", \"path_files\": $PATHS }" eda.jferre.local:5001
-   curl -X POST https://controller.jferre.local/api/v2/job_templates/9/launch/ -H "Content-Type: application/json" -H "Authorization: Bearer YOUR_TOKEN_HERE" --data "{\"extra_vars\": {\"path_file\": $PATHS } }"
+   curl -X POST https://controller.jferre.local/api/v2/job_templates/9/launch/ -H "Content-Type: application/json" -H "Authorization: Bearer uuH7feLbMYRA4Wk1DgiRb7ELaHeNjC" --data "{\"extra_vars\": {\"path_file\": $PATHS } }"
 
 fi
--- inotify.sh
+++ inotify.sh
@@ -6,6 +6,7 @@ FILE=`echo $line | awk '{print $3}'`
 
 PATHFILE=${DIRECTORY}${FILE}
 
-curl -X POST https://controller.jferre.local/api/v2/job_templates/9/launch/ -H "Content-Type: application/json" -H "Authorization: Bearer YOUR_TOKEN_CONTROLLER_HERE" --data "{\"extra_vars\": {\"path_file\": \"$PATHFILE\" } }"
+#curl -X POST https://controller.jferre.local/api/v2/job_templates/9/launch/ -H "Content-Type: application/json" -H "Authorization: Bearer uuH7feLbMYRA4Wk1DgiRb7ELaHeNjC" --data "{\"extra_vars\": {\"path_file\": \"$PATHFILE\" } }"
+curl --header "Content-Type: application/json" --request POST --data "{\"path_file\": \"$PATHFILE\" }" eda.jferre.local:5001
 
 done
